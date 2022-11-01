d4af231ff0206a13dcf8db1328f1e700b8cb2126
mirroflower <xywxd666@126.com>
2022-11-01 08:50:56 +0800

    111111111

---
+++ src/views/user/VueDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+let app
+export default {
+  name: 'vue测试',
+  data() {
+    return {
+      title: 'vue测试',
+    }
+  },
+  computed: {},
+  methods: {},
+  created() {
+    app = this
+    console.log(app.title)
+  },
+}
+</script>
